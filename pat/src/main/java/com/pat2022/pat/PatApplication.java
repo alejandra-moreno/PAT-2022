5aa05c6bb8b6c527b9868e2523954587d6eeba3a
Alejandra Moreno <moreno.grande.alejandra@gmail.com>
2022-04-29 16:41:52 +0200

    First commit

---
+++ pat/src/main/java/com/pat2022/pat/PatApplication.java
@@ -0,0 +1,13 @@
+package com.pat2022.pat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatApplication.class, args);
+	}
+
+}
---
+++ pat/src/test/java/com/pat2022/pat/PatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pat2022.pat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
